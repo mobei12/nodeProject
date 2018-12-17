e6184e201620d19d566b2b26992fff15a6f7948f
mobei12 <505453856@qq.com>
2018-12-17 12:45:59 +0800

    文件管理，添加路由转发服务

--- app.js
+++ app.js
@@ -2,10 +2,7 @@ const express = require('express');
 const app = express();
 
 const express_proxy = require('./server/express_proxy');
-
-
 app.use(express.static('html'));
-
 app.use('/express_proxy', express_proxy);
 
 module.exports = app;
