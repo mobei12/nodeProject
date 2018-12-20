0faba1ff6b1c64c30531ad3f0d6389ade2ae381e
mobei12 <505453856@qq.com>
2018-12-20 14:23:17 +0800

    文件管理，添加路由转发服务

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 const express_proxy = require('./server/express_proxy');
-app.use(express.static('html'));
 app.use('/express_proxy', express_proxy);
-
+app.get('/indexMain.html', function (req, res) {
+    res.sendFile(__dirname + "/" + "indexMain.html");
+});
 module.exports = app;
--- server/express_proxy.js
+++ server/express_proxy.js
@@ -2,10 +2,12 @@
 
 const express = require('express');
 const http = require('http');
+const https = require('http');
 //如果第三方api是https，则以上为const https = require('https')
 //下面的代码 http 处相应更改为 https，并将80端口更新为 443
 const router = express.Router();
 let _fn = {};
+let _fns = {};
 const apiHost = '';//要代理的域名地址
 
 
@@ -13,6 +15,13 @@ const apiHost = '';//要代理的域名地址
 router.get('/', function (req, res, next) {
     const path = req.originalUrl;
     _fn.getData(path, function (data) {
+        res.send(data);
+    });
+});
+//转发 get 请求
+router.get('/getWeather', function (req, res, next) {
+    const path = req.originalUrl;
+    _fns.getData(path, function (data) {
         res.send(data);
     });
 });
@@ -26,7 +35,7 @@ router.post('/', function (req, res, next) {
     });
 });
 
-_fn = {
+_fn = {//http请求
     getData: function (path, callback) {
         http.get({
             hostname: apiHost,
@@ -73,6 +82,24 @@ _fn = {
         req.write(content);
         req.end();
     }
+};
+_fns = {//https请求
+    getData: function (path, callback) {
+        https.get({
+            hostname: 'api.seniverse.com/v3/weather/now.json?key=vdqsbdu2eteerwup&location=changchun&language=zh-Hans&unit=c',
+            port: 843,//https端口号默认80
+            path: path
+        }, function (res) {
+            let body = [];
+            res.on('data', function (chunk) {
+                body.push(chunk);
+            });
+            res.on('end', function () {
+                body = Buffer.concat(body);
+                callback(body.toString());
+            });
+        });
+    },
 };
 
 module.exports = router;
